0db4f865f5b73179e0d094de60f08c040aaedf9f
Fadil Al <fadil.alfarhan@gmail.com>
2021-05-26 00:14:25 +0200

    Filter and map Real Based Senarios...

--- ES6-/fundamentals.js
+++ ES6-/fundamentals.js
@@ -22,19 +22,20 @@ const Hellleo = name => name
 // let personInfo = [['Eyad', 30], ['Fadil', 28], ['Momma', 26]]
 let fruitBag = [['banana', 23], ['strawberry', 30 ], ['Kiwi', 15]]
 
-let msgFromStore = fruitBag.map(([fruit, amount], i) => fruit + ' ' + amount + ' ' + 'index ' + (i + 1)  )
-let msgFromStoreToObj = fruitBag.map(([fruit, amount], i) => {
-
-    return {
+// let msgFromStore = fruitBag.map(([fruit, amount], i) => fruit + ' ' + amount + ' ' + 'index ' + (i + 1)  )
+let msgFromStoreToObj = fruitBag.map(([fruit, amount], i) => (
+     {
           id:i+1,
           fruitType: fruit,
           amount: amount,
      }
-})
+))
 
 
-console.log(msgFromStore)
-msgFromStoreToObj.filter((fruits => console.log(fruits.fruitType)))
+// console.log('msgFromStore: ', msgFromStore)
+let myFruits = msgFromStoreToObj.map(({fruitType}) => ({fruitType}))
+console.log('myFruits ',myFruits)
+
 
 let users = [
      {name: 'user1', score: 100, level: 5}, 
@@ -57,10 +58,6 @@ console.log('test: ',userInfoos)
 
 // names.map(function (name) { console.log(`This is a Normal function: ${name}`)})
 
-
-
-
-
 // Göra om en array till ett object
 // let writeMyName = personInfo.map(([name,age],i) =>{return {id: i, name: name, age: age}})
 // console.log(writeMyName)
@@ -71,3 +68,33 @@ console.log('test: ',userInfoos)
 // console.log(`Object.entries`, objEntries)
 // console.log(`Object.keys `,objKeys)
 // console.log(`Object.values `,objValues)
+
+let employees = [
+     {name: 'Khaled', age: 34, salary:24000, city: 'Lissabon', soldItems: 10},
+     {name: 'Bright', age: 41, salary:27000, city: 'Berlin', soldItems: 25},
+     {name: 'Fanny', age: 23, salary: 18000, city: 'Stockholm', soldItems: 15},
+     {name: 'Morgan', age: 41, salary:35000, city: 'Norge', soldItems: 20},
+   
+]
+
+let employeesAge = employees.filter(({age, salary}) => age === 41 && salary === 35000) 
+console.log(employeesAge)
+
+let bonus = employees.map(({ name, age, salary, city, soldItems},i) => {
+     
+     if(soldItems > 1 && soldItems <11) { 
+          return {name, age, salary: salary + 10, city, soldItems,  BonusMsg: `You sold ${soldItems} items, thats $10 in bonus!`}
+     }
+     if(soldItems > 10 && soldItems <16) { 
+          return {name, age, salary: salary + 15, city, soldItems,  BonusMsg: `You sold ${soldItems} items, thats $15 in bonus!`}
+     }
+
+     if(soldItems > 15 && soldItems <21) {
+          return {name, age, salary: salary + 25, city, soldItems,  BonusMsg: `You sold ${soldItems} items, thats $25 in bonus!`}
+     }
+     if(soldItems > 20 && soldItems <30) {
+          return {name, age, salary: salary + 40, city, soldItems,  BonusMsg: `You sold ${soldItems} items, thats $40 in bonus!`}
+     }
+})
+
+console.log('bonus',bonus)
