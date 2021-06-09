99b0e87ec5c3983debe7d76b7998c1ee8e302aca
Fadil Al <fadil.alfarhan@gmail.com>
2021-06-09 15:00:11 +0200

    last edited

--- traineerp/client/src/App.js
+++ traineerp/client/src/App.js
@@ -1,52 +1,13 @@
-import { useEffect, useState } from 'react'
 
-import Fadil from './components/Fadil'
-import Posts from './components/Posts'
-import Eyad from './components/Eyad'
+import TodoApp from './components/fadilProjects/todoApp/TodoApp'
 
 import './App.css';
 
 function App() {
-    let [myUsersData, setMyUsersData] = useState([])
-    
-    function addUser(e){
-      e.preventDefault()
-      
-        fetch('https://randomuser.me/api/')
-        .then((response) => response.json())
-        .then((usersData) => {
-          
-          setMyUsersData([...myUsersData, ...usersData.results])
-          
-          let test = myUsersData.reduce((acc, cur)=>{
-            return acc + cur
-          })
-          console.log(test)
-        })
-      }
-
-
-      
-      useEffect(() => {
-        console.log(myUsersData)
-        
-      }, [myUsersData]);
-    
+  
   return (
     <div className="App">
-<<<<<<< HEAD:traineerp/client/src/App.js
-    <Fadil />
-    {/* <Posts />
-      <button onClick={addUser}>Generate User</button>
-=======
-    <Posts />
-    <Eyad />
-
-      {/* <button onClick={addUser}>Generate User</button>
->>>>>>> e86d558655bae95a52958eaa0dbaa737140721f6:traineerp/src/App.js
-        {
-            myUsersData.map((user)=>(<div key={user.login.uuid}><h1>{user.name.first}</h1> <img src={user.picture.large} alt={user.name.first} /></div>))
-        } */}
+    <TodoApp />
     </div>
   );
 }
