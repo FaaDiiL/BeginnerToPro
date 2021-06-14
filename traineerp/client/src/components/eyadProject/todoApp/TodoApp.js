5ed8429110239c370dda8c9e14e97deec584603c
Fadil Al <fadil.alfarhan@gmail.com>
2021-06-14 14:55:41 +0200

    first commit

--- traineerp/client/src/components/eyadProject/todoApp/TodoApp.js
+++ traineerp/client/src/components/eyadProject/todoApp/TodoApp.js
@@ -1,65 +1,74 @@
-import React,{ useState } from 'react'
-
-// TodoApp
-     // Todo 1.0 - Form med (text-input) och (submit-knapp) / 
-     // Todo 1.2 - Lista med alla todos /  Todo (checkbox- ture? done) /  (Edit||Delete)
-     // Todo 2.0 - Spara till localStorage
-     // Todo 3.0 - läsa / skriva till localStorage
+import React, { useEffect, useState } from 'react'
 
 function TodoApp() {
+    // TodoApp
+    // Todo 1.0 - Form med (text-input) och (submit-knapp) / 
+    // Todo 1.2 - Lista med alla todos /  Todo-title buttons=>(Done||Edit||Delete)
+    // Todo 1.3 - Skapa en console.log()
+    // Todo 2.0 - Spara till localStorage
+    // Todo 3.0 - lÃ¤sa / skriva till localStorage,
 
+   
     const [todos, setTodos] = useState([])
-     const [title,setTitle] = useState('')
-     const [isDone, setIsDone] = useState(false)
+    const [title, setTitle] = useState('')
+    const [isDone, setIsDone] = useState(false)
 
-     const handleSubmit = (e) => {
-          e.preventDefault()
-          console.log('Todo Created')
-     }
-     const handleChange = (e) =>{
-          // setTitle(e.target.value)
-          console.log(e.target.value)
-     }
-     const editTodo = (e) =>{
-          console.log('Edited todo')
-     }
-     const deleteTodo = (e) =>{
-          console.log('Deleted todo')
-     }
-     const doneTodo= () => {
-          setIsDone(!isDone)
-          if(isDone !== true) {
-               console.log(`Todo is undone`)
-          }else{
-               console.log(`Todo is done`)
-          }
-     }
+    const handleChange = (e) => {
+        setTitle(e.target.value)
+    }
 
-    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setTodos([...todos, { title, isDone}])
+    }
+
+    const handleDone = (e) => {
+         e.target.checked = 
+         setIsDone(e.target.checked)
+    }
+    const handleEdit = (e) => {
+        setTodos(todos.find((todo,i) => i === +e.target.id))
+    }
+
+    const handleDelete = (e) => {
+        console.log('Todo is deleted', e.target.id)
+        setTodos(todos.filter((todo, i) => i !== +e.target.id ))
+    }
+
+
     return (
+        <>
 
-        <div>
-
-            <h1>Eyads Todo App</h1>
-
-            <form onSubmit= {handleSubmit}>
-                <input onChange={handleChange} type='text' name="input1" placeholder='todo'  /> 
-                <button>Add </button>
+            <form onSubmit={handleSubmit}>
+                <input type='text' onChange={handleChange} />
+                <button>Add</button>
             </form>
-            
-            <ul >
-                <li>
-                    <span style={{fontSize: '2rem', color: 'black'}}>
-                                    My first todo
-                                    <i className="fas fa-check" onClick={doneTodo} />
-                                    <i className="far fa-edit " onClick={editTodo} />
-                                    <i className="fas fa-trash-alt" onClick={deleteTodo} />
-                    </span>
-                </li>
+
+            <ul>
+            {
+                 todos.map(({title, isDone},id)=>(
+                     <li key={id}>
+                         <span style={{ fontSize: '2rem' }}>
+                              {title}
+                              <input type='checkbox' onChange={handleDone} defaultChecked={isDone} />
+                              <i className="far fa-edit " onClick={handleEdit} />
+                              <i className="fas fa-trash-alt" onClick={handleDelete} id={id} />
+                         </span>
+                    </li>
+                 )
+                      
+                )
+            }
+               
             </ul>
 
-        </div>
+        </>
+
+
+
     )
+
+     
 }
 
 export default TodoApp
