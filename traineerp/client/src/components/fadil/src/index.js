688311f9054ea16f32f74c119afe911cde66c136
Fadil Al <fadil.alfarhan@gmail.com>
2021-08-02 19:42:10 +0200

    initial app

--- traineerp/client/src/components/fadil/src/index.js
+++ traineerp/client/src/components/fadil/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import '@fontsource/roboto'; // Defaults to weight 400.
+import { ThemeContext } from './Components/context/context';
+const dark = {color: 'darkRed', backgroundColor: 'red'}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext.Provider value={dark} >
+      <App />
+    </ThemeContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
