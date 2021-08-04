1c9ccba75bdd825c3c46f0f2aada0596603f0623
Fadil Al <fadil.alfarhan@gmail.com>
2021-08-04 12:14:27 +0200

    Created a function that give us the value of the pressed button and created states for the calculator

--- traineerp/client/src/components/calculatorf/src/Components/Calculator/CalculatorKeys/Operators/Operators.js
+++ traineerp/client/src/components/calculatorf/src/Components/Calculator/CalculatorKeys/Operators/Operators.js
@@ -1,14 +1,24 @@
 import React from 'react'
+
 import './Operators.css'
+
 function Operators() {
-     return (
-          <>
-               <button type="button" className="operator" value="+">+</button>
-               <button type="button" className="operator" value="-">-</button>
-               <button type="button" className="operator" value="*">×</button>
-               <button type="button" className="operator" value="/">÷</button>
-          </>
-     )
+  return (
+    <>
+      <button type='button' className='operator' value='+'>
+        +
+      </button>
+      <button type='button' className='operator' value='-'>
+        -
+      </button>
+      <button type='button' className='operator' value='*'>
+        ×
+      </button>
+      <button type='button' className='operator' value='/'>
+        ÷
+      </button>
+    </>
+  )
 }
 
 export default Operators
