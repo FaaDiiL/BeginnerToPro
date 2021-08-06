4c2f6c959b281140c4626076acbec34762d6e011
Fadil Al <fadil.alfarhan@gmail.com>
2021-08-06 20:12:23 +0200

    Added useStates, added logic for the calculator, Used if-statements and switch.

--- traineerp/client/src/components/calculatorf/src/Components/Calculator/Calculator.js
+++ traineerp/client/src/components/calculatorf/src/Components/Calculator/Calculator.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import CalculatorKeys from './CalculatorKeys/CalculatorKeys'
 import CalculatorScreen from './CalculatorScreen/CalculatorScreen'
@@ -6,20 +6,65 @@ import CalculatorScreen from './CalculatorScreen/CalculatorScreen'
 import './Calculator.css'
 
 function Calculator() {
-  const [firstPart, setFirstPart] = useState(0)
-  const [operator, setOperator] = useState(null)
-  const [secondPart, setSecondPart] = useState(0)
+  const [tapedNumbers, setTapedNumbers] = useState([])
+  const [firstPart, setFirstPart] = useState({ number: 0, completed: false })
+  const [operator, setOperator] = useState({ operator: null, completed: false })
+  const [secondPart, setSecondPart] = useState({ number: 0, completed: false })
+  const [screen, setScreen] = useState()
   const [result, setResult] = useState(null)
 
   const getButtonValue = (e) => {
-    if (e.target.value !== undefined) {
-      console.log(e.target.value)
+    if (e.target.value === undefined) {
+      return
+    }
+    if (
+      e.target.className === 'number-btn' &&
+      e.target.className !== 'operator'
+    ) {
+      setTapedNumbers(Number([tapedNumbers, e.target.value].join('')))
+      setScreen(Number([tapedNumbers, e.target.value].join('')))
+    }
+    if (e.target.className === 'operator' && !operator.completed) {
+      setOperator({ operator: e.target.value, completed: true })
+      setFirstPart({
+        number: Number([tapedNumbers].join('')),
+        completed: true,
+      })
+      setTapedNumbers(Number(null))
+    }
+    if (e.target.className === 'equal-sign' && operator.completed) {
+      setSecondPart({
+        number: Number([tapedNumbers].join('')),
+        completed: true,
+      })
+
+      switch (operator.operator) {
+        case '+':
+          setResult(firstPart.number + tapedNumbers)
+          setScreen(firstPart.number + tapedNumbers)
+          break
+        case '-':
+          setResult(firstPart.number - tapedNumbers)
+          setScreen(firstPart.number - tapedNumbers)
+          break
+        case '*':
+          setResult(firstPart.number * tapedNumbers)
+          setScreen(firstPart.number * tapedNumbers)
+          break
+        case '/':
+          setResult(firstPart.number / tapedNumbers)
+          setScreen(firstPart.number / tapedNumbers)
+          break
+
+        default:
+          break
+      }
     }
   }
 
   return (
     <div className='calculator'>
-      <CalculatorScreen />
+      <CalculatorScreen screen={screen} />
       <div onClick={getButtonValue}>
         <CalculatorKeys />
       </div>
