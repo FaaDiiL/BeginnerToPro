c11afb0ef1d8d5f42bf232ad9afa20048376de42
eyadal <31968847+eyadal@users.noreply.github.com>
2021-08-16 11:47:20 +0200

    import material ui and add a Container

--- portfolioe/src/App.js
+++ portfolioe/src/App.js
@@ -3,14 +3,15 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import { Container } from '@material-ui/core'
 
 function App() {
   return (
-    <div className='wrapper'>
+    <Container className='wrapper'>
       <Header />
       <Main />
       <Footer />
-    </div>
+    </Container>
   )
 }
 
