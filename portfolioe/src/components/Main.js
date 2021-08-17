c9316af5f362142c8e8d8151218c3196782555b4
eyadal <31968847+eyadal@users.noreply.github.com>
2021-08-17 10:30:08 +0200

    edited (section deleted)

--- portfolioe/src/components/Main.js
+++ portfolioe/src/components/Main.js
@@ -4,33 +4,27 @@ import Contact from './Contact'
 import WorkExperience from './WorkExperience'
 import Education from './Education'
 import Bio from './Bio'
+import { Grid, Typography } from '@material-ui/core'
 
 function Main() {
   return (
-    <main>
-      <section id='Bio' />
-      <Bio />
-      <section />
+    <div>
+      <Grid container>
+        <Bio />
+        <WorkExperience />
+        <Education />
 
-      <section id='WorkExperience' />
-      <WorkExperience />
-      <section />
-
-      <section id='Education' />
-      <Education />
-      <section />
-
-      <section id='Portfolio' />
-      <h3>Last Edited Projects</h3>
-      <Project last={3} />
-      <h3>All Projects</h3>
-      <Project />
-      <section />
-
-      <section id='Contact' />
-      <Contact />
-      <section />
-    </main>
+        <Typography variant='h4' color='textPrimary' component='h4'>
+          Last Edited Projects
+        </Typography>
+        <Project last={3} />
+        <Typography variant='h4' color='textPrimary' component='h4'>
+          All Projects
+        </Typography>
+        <Project />
+        <Contact />
+      </Grid>
+    </div>
   )
 }
 
